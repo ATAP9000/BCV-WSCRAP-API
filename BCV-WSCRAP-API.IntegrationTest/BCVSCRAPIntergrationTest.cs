d7eb96f206dcfa12da287893d51f796046bf91e5
Angelo T <atap9000@gmail.com>
2024-03-19 00:49:49 -0400

    feat: added Basic Integration Test

---
+++ BCV-WSCRAP-API.IntegrationTest/BCVSCRAPIntergrationTest.cs
@@ -0,0 +1,35 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+
+namespace BCV_WSCRAP_API.IntegrationTest
+{
+    public class BCVSCRAPIntergrationTest : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly WebApplicationFactory<Program> _factory;
+
+        public BCVSCRAPIntergrationTest(WebApplicationFactory<Program> factory)
+        {
+            _factory = factory;
+        }
+
+        [Theory]
+        [InlineData("/BCVSCRAP")]
+        [InlineData("/BCVSCRAP/CurrentExchangeRate")]
+        [InlineData("/BCVSCRAP/RecentIntervention")]
+        [InlineData("/BCVSCRAP/Interventions")]
+        [InlineData("/BCVSCRAP/BankRates")]
+        public async Task Get_EndpointsReturnSuccessAndCorrectContentType(string url)
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync(url);
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+
+    }
+}
