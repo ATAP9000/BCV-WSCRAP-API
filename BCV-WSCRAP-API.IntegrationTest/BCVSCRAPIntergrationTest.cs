4bc27c9455886078d27a5491ea5c5c4a499b13e8
ATAP <atap9000@outlook.com>
2026-04-02 11:29:52 -0400

    adjust with agent user in integration

--- BCV-WSCRAP-API.IntegrationTest/BCVSCRAPIntergrationTest.cs
+++ BCV-WSCRAP-API.IntegrationTest/BCVSCRAPIntergrationTest.cs
@@ -13,23 +13,26 @@ namespace BCV_WSCRAP_API.IntegrationTest
             _factory = factory;
         }
 
-        // [Theory]
-        // [InlineData("/BCVSCRAP")]
-        // [InlineData("/BCVSCRAP/CurrentExchangeRate")]
-        // [InlineData("/BCVSCRAP/ExchangeRates")]
-        // [InlineData("/BCVSCRAP/RecentIntervention")]
-        // [InlineData("/BCVSCRAP/Interventions")]
-        // [InlineData("/BCVSCRAP/BankRates")]
-        // public async Task Get_EndpointsReturnSuccessAndCorrectContentType(string url)
-        // {
-        //     // Arrange
-        //     var client = _factory.CreateClient();
+        [Theory]
+        [InlineData("/BCVSCRAP")]
+        [InlineData("/BCVSCRAP/CurrentExchangeRate")]
+        [InlineData("/BCVSCRAP/ExchangeRates")]
+        [InlineData("/BCVSCRAP/RecentIntervention")]
+        [InlineData("/BCVSCRAP/Interventions")]
+        [InlineData("/BCVSCRAP/BankRates")]
+        public async Task Get_EndpointsReturnSuccessAndCorrectContentType(string url)
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+            string customUserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36";
+            
+            client.DefaultRequestHeaders.Add("User-Agent", customUserAgent);
 
-        //     // Act
-        //     var response = await client.GetAsync(url);
+            // Act
+            var response = await client.GetAsync(url);
 
-        //     // Assert
-        //     response.StatusCode.Should().Be(HttpStatusCode.OK);
-        // }
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
     }
 }
